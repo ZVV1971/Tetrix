ceb6bd7d4ee0e9e177f664454f850d547047da92
ZVV1971 <Uladzimir_Zakharenkau@epam.com>
2024-02-11 11:19:25 +0100

    Add new figure Cross and correspondin tests

---
+++ TetrisFigures/TetrisCrossControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using TetrisFigures.Interfaces;
+
+namespace TetrisFigures
+{
+    /// <summary>
+    /// Interaction logic for TetrisCrossControl.xaml
+    /// </summary>
+    public partial class TetrisCrossControl : TetrisUserControl
+    {
+        public TetrisCrossControl()
+        {
+            InitialPosition = new List<Tuple<int, int>>()
+            {
+                new Tuple<int, int>(-1, -1),
+                new Tuple<int, int>(0, -1),
+                new Tuple<int, int>(1, -1),
+                new Tuple<int, int>(0, 0),
+                new Tuple<int, int>(0, -2)
+            };
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public override void ChangeSize(int size)
+        {
+            CrossControlWindow.Width = size * 3;
+            CrossControlWindow.Height = size * 3;
+        }
+    }
+}
---
+++ TetrisTests/Figures/Cross/CrossTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using TetrisFigures;
+using TetrisFigures.Interfaces;
+
+namespace TetrisTests.Figures.Cross
+{
+    internal class CrossTests
+    {
+        public TetrisUserControl ctrl;
+
+        [SetUp]
+        public void Setup()
+        {
+            ctrl = new TetrisCrossControl();
+        }
+
+        [TestCaseSource(nameof(CrossPositions)), Description("Test whether Cross rotation gives the original Cross position")]
+        [Apartment(ApartmentState.STA)]
+        public void TestCrossRotation(List<Tuple<int, int>> pos)
+        {
+            Assert.AreEqual(pos, ctrl.Rotate(pos));
+        }
+
+        public static object[] CrossPositions =
+        {
+            new object[]
+            {
+                new List<Tuple<int, int>>()
+                {
+                    new Tuple<int, int>(9, 9),
+                    new Tuple<int, int>(10, 10),
+                    new Tuple<int, int>(10, 9),
+                    new Tuple<int, int>(10, 8),
+                    new Tuple<int, int>(11, 9)
+                }
+            }
+        };
+    }
+}
