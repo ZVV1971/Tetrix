a6bc0164d865610f397fd05aab334efa9fbbc0d1
Uladzimir Zakharenkau <38497910+ZVV1971@users.noreply.github.com>
2024-02-16 14:36:37 +0100

    Feature/52 make the game stoppable and resumable (#54)
    * Create MultiConverter
    Add stoppability to the game
    
    * Do not generate PDBs in Release Config for test project

---
+++ TetrisFigures/Converters/MultiBoolToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TetrisFigures.Converters
+{
+    public class MultiBoolToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type sourceType, object parameter, CultureInfo culture)
+        {
+            foreach(object o in values)
+            {
+                if ((bool)o)
+                {
+                    return System.Windows.Visibility.Visible;
+                }
+            }
+            return System.Windows.Visibility.Hidden;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
