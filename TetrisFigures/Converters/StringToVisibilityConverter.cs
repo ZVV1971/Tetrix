2a3aa316e84dcb459359aab17d017f99f9806ff7
Uladzimir Zakharenkau <38497910+ZVV1971@users.noreply.github.com>
2024-04-20 12:08:10 +0200

    Feature/67 add current gamer info (#68)
    * Add string to visibility converter to be able to decide whether to show string values if they're empty or not.
    
    * Add current user indication to the main window
    Remove Username dialog since user from now on is always defined.

---
+++ TetrisFigures/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TetrisFigures.Converters
+{
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)value != null && !((string)value).Equals(string.Empty))
+            {
+                return System.Windows.Visibility.Visible;
+            }
+
+            return System.Windows.Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
