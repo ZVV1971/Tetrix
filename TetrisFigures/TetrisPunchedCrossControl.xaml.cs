1ceaf5d8c6f51be3f000dd437e4fcaa1bcf4d5ec
Uladzimir Zakharenkau <38497910+ZVV1971@users.noreply.github.com>
2024-11-17 16:34:34 +0100

    - Add figure "Punched Cross"; (#75)

---
+++ TetrisFigures/TetrisPunchedCrossControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using TetrisFigures.Interfaces;
+
+namespace TetrisFigures
+{
+    /// <summary>
+    /// Interaction logic for TetrisPunchedCrossControl.xaml
+    /// </summary>
+    public partial class TetrisPunchedCrossControl : TetrisUserControl
+    {
+        public TetrisPunchedCrossControl()
+        {
+            InitialPosition = new List<Tuple<int, int>>()
+            {
+                new Tuple<int, int>(-1, 0),
+                new Tuple<int, int>(0, -1),
+                new Tuple<int, int>(1, 0),
+                new Tuple<int, int>(0, 1)
+            };
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public override void ChangeSize(int size)
+        {
+            PunchedCrossControlWindow.Width = size * 3;
+            PunchedCrossControlWindow.Height = size * 3;
+        }
+    }
+}
---
+++ TetrisTests/Figures/PunchedCross/PunchedCrossTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using TetrisFigures;
+using TetrisFigures.Interfaces;
+
+namespace TetrisTests.Figures.PunchedCross
+{
+    internal class PunchedCrossTests
+    {
+        public TetrisUserControl ctrl;
+
+        [SetUp]
+        public void Setup()
+        {
+            ctrl = new TetrisPunchedCrossControl();
+        }
+
+        [TestCaseSource(nameof(PunchedCrossPositions)), Description("Test whether PucnhedCross rotation gives the original PunchedCross position")]
+        [Apartment(ApartmentState.STA)]
+        public void TestCrossRotation(IList<Tuple<int, int>> pos)
+        {
+            Assert.AreEqual(pos, ctrl.Rotate(pos));
+        }
+
+        public static object[] PunchedCrossPositions =
+        {
+            new object[]
+            {
+                new List<Tuple<int, int>>()
+                {
+                    new Tuple<int, int>(9, 9),
+                    new Tuple<int, int>(10, 10),
+                    new Tuple<int, int>(10, 8),
+                    new Tuple<int, int>(11, 9)
+                }
+            }
+        };
+    }
+}
