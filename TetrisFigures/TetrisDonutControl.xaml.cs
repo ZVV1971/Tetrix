d4d9d917678ea713dc4fb9c8cf85e01a05bee10b
Uladzimir Zakharenkau <38497910+ZVV1971@users.noreply.github.com>
2024-11-17 13:02:32 +0100

    Feature/69 add donut figure (#70)
    * Change Checkout options in the YAML set-safe-directory: false
    
    * Change Checkout options in the YAML set-safe-directory: false to main branch
    
    * - Add donut figure;
    - Add test on donut figure postioning;

---
+++ TetrisFigures/TetrisDonutControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using TetrisFigures.Interfaces;
+
+namespace TetrisFigures
+{
+    /// <summary>
+    /// Interaction logic for TetrisDonutControl.xaml
+    /// </summary>
+    public partial class TetrisDonutControl : TetrisUserControl
+    {
+        public TetrisDonutControl()
+        {
+            InitialPosition = new List<Tuple<int, int>>()
+            {
+                new Tuple<int, int>(-1, -1),
+                new Tuple<int, int>(0, -1),
+                new Tuple<int, int>(1, -1),
+                new Tuple<int, int>(-1, 0),
+                new Tuple<int, int>(1, 0),
+                new Tuple<int, int>(-1, 1),
+                new Tuple<int, int>(0, 1),
+                new Tuple<int, int>(1, 1),
+            };
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public override void ChangeSize(int size)
+        {
+            DonutControlWindow.Width = size * 3;
+            DonutControlWindow.Height = size * 3;
+        }
+    }
+}
