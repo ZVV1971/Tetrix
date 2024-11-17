74a89ca3617cc5fb02adfc4833aed62e9d4f1b71
Uladzimir Zakharenkau <38497910+ZVV1971@users.noreply.github.com>
2024-11-17 18:45:16 +0100

    Feature/72 add figure bird (#76)
    * - Add bird figure;
    
    * - Add test for Bird figure;

---
+++ TetrisFigures/TetrisBirdControl.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using TetrisFigures.Interfaces;
+
+namespace TetrisFigures
+{
+    /// <summary>
+    /// Interaction logic for BirdCrossControl.xaml
+    /// </summary>
+    public partial class TetrisBirdControl : TetrisUserControl
+    {
+        public TetrisBirdControl()
+        {
+            TransformationMatrix = new List<Tuple<int, int>[]>()
+            {
+                //transformation for position #0 -- initial horizontal position 0 degrees rotation
+                new Tuple<int, int>[] {
+                    new Tuple<int, int> (1, -1),  //-point #0
+                    new Tuple<int, int> (-1, 1),  //-point #1
+                    new Tuple<int, int> (-1, -1)  //-point #2
+                },
+                //transformation for position #1 -- vertical position 90 degrees rotation
+                new Tuple<int, int>[] {
+                    new Tuple<int, int> (1, 1),   //-point #0
+                    new Tuple<int, int> (-1, -1), //-point #1
+                    new Tuple<int, int> (1, -1)   //-point #2
+                },
+                //transformation for position #2 -- invert horizontal position 180 degrees rotation
+                new Tuple<int, int>[] {
+                    new Tuple<int, int> (-1, 1),   //-point #0
+                    new Tuple<int, int> (1, -1),   //-point #1
+                    new Tuple<int, int> (1, 1)     //-point #2
+                },
+                //transformation for position #3 -- invert vertical position 270 degrees rotation
+                new Tuple<int, int>[] {
+                    new Tuple<int, int> (-1, -1),  //-point #0
+                    new Tuple<int, int> (1, 1),    //-point #1
+                    new Tuple<int, int> (-1, 1)    //-point #2
+                }
+            };
+            InitialPosition = new List<Tuple<int, int>>()
+            {
+                new Tuple<int, int>(-1, -1),
+                new Tuple<int, int>(1, -1),
+                new Tuple<int, int>(0, 0)
+            };
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public override void ChangeSize(int size)
+        {
+            BirdControlWindow.Width = size * 3;
+            BirdControlWindow.Height = size * 2;
+        }
+
+        public override int GetPositionNumber(IList<Tuple<int, int>> tuples)
+        {
+            if (tuples[0].Item2 == tuples[1].Item2)
+            //positions #0 and #2 -- 0 and 180 degrees
+            {
+                if (tuples[1].Item1 > tuples[2].Item1)
+                    //position 0 degrees #0
+                    return 0;
+                else
+                    //position 180 degrees #2
+                    return 2;
+            }
+            else
+            //positions #1 and #3 -- 90 and 270 degrees
+            {
+                if (tuples[1].Item1 < tuples[2].Item1)
+                    //position 270 degrees #3
+                    return 3;
+                else
+                    //position 90 degrees #1
+                    return 1;
+            }
+        }
+    }
+}
---
+++ TetrisTests/Figures/Bird/BirdTests.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using TetrisFigures.Interfaces;
+using TetrisFigures;
+using System.Threading;
+
+namespace TetrisTests.Figures.Bird
+{
+    internal class BirdPositionTests
+    {
+        public TetrisUserControl ctrl;
+
+        [SetUp]
+        public void Setup()
+        {
+            ctrl = new TetrisBirdControl();
+        }
+
+        [TestCaseSource(nameof(BirdPositions)), Description("Test how Bird figure defines positions")]
+        [Apartment(ApartmentState.STA)]
+        public void TestBirdPositions(IList<Tuple<int, int>> pos, int res)
+        {
+            Assert.AreEqual(res, ctrl.GetPositionNumber(pos));
+        }
+
+        [TestCaseSource(nameof(BirdPositions)), Description("Test that Bird figure rotated from one to another position gets unequal position numbers")]
+        [Apartment(ApartmentState.STA)]
+        public void TestBirdRotation_OldPos_nonEqual_NewPos(IList<Tuple<int, int>> pos, int d)
+        {
+            Assert.AreNotEqual(ctrl.GetPositionNumber(pos), ctrl.GetPositionNumber(ctrl.Rotate(pos)));
+        }
+
+        [TestCaseSource(nameof(BirdPositions)), Description("Test that Bird figure rotated from one to another positions get position numbers deffering in 1 or 3")]
+        [Apartment(ApartmentState.STA)]
+        public void TestBirdRotation_OldPos_NewPos_diffc_in_1(IList<Tuple<int, int>> pos, int d)
+        {
+            int posnum = ctrl.GetPositionNumber(pos);
+            int res = posnum - ctrl.GetPositionNumber(ctrl.Rotate(pos));
+
+            if (posnum == 3)
+                Assert.That(res == 3);
+            else
+                Assert.That(res == -1);
+        }
+
+        [TestCaseSource(nameof(BirdPositions)), Description("Test that Bird figure being four times rotated returns in the same position")]
+        [Apartment(ApartmentState.STA)]
+        public void TestBirdRotation_DoubleRotation_theSame(IList<Tuple<int, int>> pos, int d)
+        {
+            Assert.AreEqual(ctrl.GetPositionNumber(pos), ctrl.GetPositionNumber(ctrl.Rotate(ctrl.Rotate(ctrl.Rotate(ctrl.Rotate(pos))))));
+        }
+
+        [TestCaseSource(nameof(BirdPositions)), Description("Test that Bird figure being rotated returns in the next position")]
+        [Apartment(ApartmentState.STA)]
+        public void TestBirdRotation_SingleRotation_theNext(IList<Tuple<int, int>> pos, int d)
+        {
+            Assert.AreEqual((d + 1) % BirdPositions.Length, ctrl.GetPositionNumber(ctrl.Rotate(pos)));
+        }
+
+        public static object[] BirdPositions =
+        {
+            //Initial position #0
+            new object[]
+            {
+                new List<Tuple<int, int>>()
+                    {
+                    new Tuple<int, int>(1, 1),
+                    new Tuple<int, int>(3, 1),
+                    new Tuple<int, int>(2, 2)
+                    },
+                    0
+            },
+            //Vertical position #1
+            new object[]
+            {
+                new List<Tuple<int, int>>()
+                    {
+                    new Tuple<int, int>(1, 1),
+                    new Tuple<int, int>(1, 3),
+                    new Tuple<int, int>(0, 2)
+                    },
+                    1
+            },
+            //Horizontal position #2
+            new object[]
+            {
+                new List<Tuple<int, int>>()
+                    {
+                    new Tuple<int, int>(3, 1),
+                    new Tuple<int, int>(1, 1),
+                    new Tuple<int, int>(2, 0)
+                    },
+                    2
+            },
+            //Inverted vertical position #3
+            new object[]
+            {
+                new List<Tuple<int, int>>()
+                    {
+                    new Tuple<int, int>(1, 3),
+                    new Tuple<int, int>(1, 1),
+                    new Tuple<int, int>(2, 2)
+                    },
+                    3
+            }
+        };
+    }
+}
