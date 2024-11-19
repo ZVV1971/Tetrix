14c54ef75c6b16db9d3eb11d2d71ea88624d0308
Uladzimir Zakharenkau <38497910+ZVV1971@users.noreply.github.com>
2024-11-19 18:30:20 +0100

    Feature/73 add figure right twist (#79)
    * - Add Right twist figure;
    
    * - Add tests for Right Twist figure;

---
+++ TetrisFigures/TetrisRightTwistControl.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using TetrisFigures.Interfaces;
+
+namespace TetrisFigures
+{
+    /// <summary>
+    /// Interaction logic for TetrisRightTwistControl.xaml
+    /// </summary>
+    public partial class TetrisRightTwistControl : TetrisUserControl
+    {
+        public TetrisRightTwistControl()
+        {
+            TransformationMatrix = new List<Tuple<int, int>[]>()
+            {
+                //transformation for position #0 -- horizontal position
+                new Tuple<int, int>[] {
+                    new Tuple<int, int> (0, -2),  //-point #0
+                    new Tuple<int, int> (1, -1),  //-point #1 
+                    new Tuple<int, int> (0, 0),   //-point #2
+                    new Tuple<int, int> (-1, 1),  //-point #3
+                    new Tuple<int, int> (0, 2)    //-point #4
+                },
+                //transformation for position #1 -- vertical position
+                new Tuple<int, int>[] {
+                    new Tuple<int, int> (0, 2),   //-point #0
+                    new Tuple<int, int> (-1, 1),  //-point #1 
+                    new Tuple<int, int> (0, 0),   //-point #2
+                    new Tuple<int, int> (1, -1),  //-point #3
+                    new Tuple<int, int> (0, -2)   //-point #4
+                }
+            };
+            InitialPosition = new List<Tuple<int, int>>()
+            {
+                new Tuple<int, int>(-1, 1),
+                new Tuple<int, int>(-1, 0),
+                new Tuple<int, int>(0, 0),
+                new Tuple<int, int>(1, 0),
+                new Tuple<int, int>(1, -1)
+            };
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public override void ChangeSize(int size)
+        {
+            RightTwistControlWindow.Width = size * 3;
+            RightTwistControlWindow.Height = size * 3;
+        }
+
+        public override int GetPositionNumber(IList<Tuple<int, int>> tuples)
+        {
+            //horizontal placement
+            if (tuples[1].Item2 == tuples[2].Item2)
+                return 0;
+            //vertical placement
+            else
+                return 1;
+        }
+    }
+}
---
+++ TetrisTests/Figures/RightTwist/RightTwistTests.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using TetrisFigures.Interfaces;
+using TetrisFigures;
+using System.Threading;
+
+namespace TetrisTests.Figures.RightTwist
+{
+    internal class PositionTests
+    {
+        public TetrisUserControl ctrl;
+
+        [SetUp]
+        public void Setup()
+        {
+            ctrl = new TetrisRightTwistControl();
+        }
+
+        [TestCaseSource(nameof(RightTwistPositions)), Description("Test how RightTwist figure defines positions")]
+        [Apartment(ApartmentState.STA)]
+        public void TestRightTwistPositions(IList<Tuple<int, int>> pos, int res)
+        {
+            Assert.AreEqual(res, ctrl.GetPositionNumber(pos));
+        }
+
+        [TestCaseSource(nameof(RightTwistPositions)), Description("Test that RightTwist figure rotated from one to another position gets unequal position numbers")]
+        [Apartment(ApartmentState.STA)]
+        public void TestRightTwistRotation_OldPos_nonEqual_NewPos(IList<Tuple<int, int>> pos, int d)
+        {
+            Assert.AreNotEqual(ctrl.GetPositionNumber(pos), ctrl.GetPositionNumber(ctrl.Rotate(pos)));
+        }
+
+        [TestCaseSource(nameof(RightTwistPositions)), Description("Test that RightTwist figure rotated from one to another positions get position numbers deffering in 1")]
+        [Apartment(ApartmentState.STA)]
+        public void TestRightTwistRotation_OldPos_NewPos_diffc_in_1(IList<Tuple<int, int>> pos, int d)
+        {
+            Assert.That(Math.Abs(ctrl.GetPositionNumber(pos) - ctrl.GetPositionNumber(ctrl.Rotate(pos))) == 1);
+        }
+
+        [TestCaseSource(nameof(RightTwistPositions)), Description("Test that RightTwist figure being twice rotated returns in the same position")]
+        [Apartment(ApartmentState.STA)]
+        public void TestRightTwistRotation_DoubleRotation_theSame(IList<Tuple<int, int>> pos, int d)
+        {
+            Assert.AreEqual(ctrl.GetPositionNumber(pos), ctrl.GetPositionNumber(ctrl.Rotate(ctrl.Rotate(pos))));
+        }
+
+        [TestCaseSource(nameof(RightTwistPositions)), Description("Test that RightTwist figure being rotated returns in the next position")]
+        [Apartment(ApartmentState.STA)]
+        public void TestRightTwistRotation_SingleRotation_theNext(IList<Tuple<int, int>> pos, int d)
+        {
+            Assert.AreEqual((d + 1) % RightTwistPositions.Length, ctrl.GetPositionNumber(ctrl.Rotate(pos)));
+        }
+
+        public static object[] RightTwistPositions =
+        {
+            //Initial position #0
+            new object[]
+            {
+                new List<Tuple<int, int>>()
+                    {
+                    new Tuple<int, int>(1, 3),
+                    new Tuple<int, int>(1, 2),
+                    new Tuple<int, int>(2, 2),
+                    new Tuple<int, int>(3, 2),
+                    new Tuple<int, int>(3, 1)
+                    },
+                    0
+            },
+            //Vertical position #1
+            new object[]
+            {
+                new List<Tuple<int, int>>()
+                    {
+                    new Tuple<int, int>(1, 1),
+                    new Tuple<int, int>(2, 1),
+                    new Tuple<int, int>(2, 2),
+                    new Tuple<int, int>(2, 3),
+                    new Tuple<int, int>(3, 3)
+                    },
+                    1
+            }
+        };
+    }
+}
